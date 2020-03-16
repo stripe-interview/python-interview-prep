798fad4efd07aedfce8bbf7c969aecbf8c796463
Adam Sorkin <azsorkin@stripe.com>
2020-03-16 11:17:30 -0700

    stripe <- Stripe

--- verify_tls.py
+++ verify_tls.py
@@ -5,7 +5,7 @@ def main():
     version = requests.get('https://www.howsmyssl.com/a/check').json()['tls_version']
     if version < "TLS 1.2":
         url = "http://pyfound.blogspot.com/2017/01/time-to-upgrade-your-python-tls-v12.html"
-        print(f"Error: The stripe API requires TLS version 1.2, you are running {version}"
+        print(f"Error: The Stripe API requires TLS version 1.2, you are running {version}"
               f"\n\nPlease see {url} for instructions to update your environment.")
         exit(1)
     print("TLS 1.2 supported, no action required.")
