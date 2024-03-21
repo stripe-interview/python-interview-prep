10da13772d0beb273a78d96ead06c46af701ec3f
onedikshathakur <onedikshathakur@gmail.com>
2024-03-21 17:02:31 +0530

    print rating

--- verify_tls.py
+++ verify_tls.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 import requests
-
 def main():
-    version = requests.get('https://www.howsmyssl.com/a/check').json()['tls_version']
+    result = requests.get('https://www.howsmyssl.com/a/check').json()
+    version = result['tls_version']
+    rating = result['rating']
+
     if version < "TLS 1.2":
         url = "https://pyfound.blogspot.com/2017/01/time-to-upgrade-your-python-tls-v12.html"
         print("Error: The Stripe API requires TLS version 1.2, you are running {version}"
@@ -10,6 +12,7 @@ def main():
                   version=version, url=url))
         exit(1)
     print("TLS 1.2 supported; no action required.")
+    print("Rating is ", rating)
 
 if __name__ == "__main__":
     main()
